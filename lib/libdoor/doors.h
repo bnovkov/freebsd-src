78a2c19276effbd0f4a952dcd65149b1ae49cd73
Paolo Lanča <paolo.lanca96@gmail.com>
2024-04-25 22:21:55 +0200

    Fix syntax for prototypes

--- lib/libdoor/doors.h
+++ lib/libdoor/doors.h
@@ -5,15 +5,17 @@ typedef struct {
 	int y;
 } foo_t;
 
-struct door_arg_t;
-struct door_info;
+typedef struct door_arg_t door_arg_t;
+typedef struct door_info door_info_t;
+typedef struct door_desc_t door_desc_t;
+typedef void door_server_func(door_info_t *);
 
 #define DOORSIOC_RETURN _IOR('a', 1, foo_t)
 #define DOORSIOC_CALL _IOW('a', 2, foo_t)
 
 int door_create(void (*server_procedure)(void *cookie, char *argp,
-		    size_t arg_size, door_desc_t *dp, uint_t n_desc),
-    void *cookie, uint_t attributes);
+		    size_t arg_size, door_desc_t *dp, u_int n_desc),
+    void *cookie, u_int attributes);
 
 int door_bind(int did);
 
@@ -22,10 +24,10 @@ int door_unbind(void);
 int door_call(int d, door_arg_t *params);
 
 int door_return(char *data_ptr, size_t data_size, door_desc_t *desc_ptr,
-    uint_t num_desc);
+    u_int num_desc);
 
 int door_info(int d, struct door_info *info);
 
 int door_revoke(int d);
 
-void (*)(door_info_t *) door_server_create(void (*create_proc)(door_info_t *));
+door_server_func *door_server_create(void (*create_proc)(door_info_t *));
--- lib/libdoor/doorsclient.c
+++ lib/libdoor/doorsclient.c
@@ -7,15 +7,19 @@
 
 int
 door_create(void (*server_procedure)(void *cookie, char *argp, size_t arg_size,
-		door_desc_t *dp, uint_t n_desc),
-    void *cookie, uint_t attributes)
+		door_desc_t *dp, u_int n_desc),
+    void *cookie, u_int attributes)
 {
+	(void)server_procedure;
+	(void)cookie;
+	(void)attributes;
 	return -1;
 }
 
 int
 door_bind(int did)
 {
+	(void)did;
 	return -1;
 }
 
@@ -28,29 +32,40 @@ door_unbind(void)
 int
 door_call(int d, door_arg_t *params)
 {
+	(void)d;
+	(void)params;
 	return -1;
 }
 
 int
 door_return(char *data_ptr, size_t data_size, door_desc_t *desc_ptr,
-    uint_t num_desc)
+    u_int num_desc)
 {
+	(void)data_ptr;
+	(void)data_size;
+	(void)desc_ptr;
+	(void)num_desc;
 	return -1;
 }
 
 int
 door_info(int d, struct door_info *info)
 {
+	(void)d;
+	(void)info;
 	return -1;
 }
 
 int
 door_revoke(int d)
 {
+	(void)d;
 	return -1;
 }
 
-void (*)(door_info_t *) door_server_create(void (*create_proc)(door_info_t *))
+door_server_func *
+door_server_create(void (*create_proc)(door_info_t *))
 {
+	(void)create_proc;
 	return NULL;
 }
