7a78d87edb987c7ea9f0ea70895ec1b7b543a1f2
MarkoVlaic <vlaic.marko@gmail.com>
2024-07-31 12:02:06 +0200

    get address of stub

--- sys/cddl/dev/sdt/sdt.c
+++ sys/cddl/dev/sdt/sdt.c
@@ -454,8 +454,8 @@ sdt_unload(void)
 	EVENTHANDLER_DEREGISTER(kld_load, sdt_kld_load_tag);
 	EVENTHANDLER_DEREGISTER(kld_unload_try, sdt_kld_unload_try_tag);
 
-	sdt_probe_func = sdt_probe_stub;
-    sdt_probe6_func = (sdt_probe6_func_t)sdt_probe_stub;
+	sdt_probe_func = &sdt_probe_stub;
+    sdt_probe6_func = (sdt_probe6_func_t)&sdt_probe_stub;
 
 	TAILQ_FOREACH_SAFE(prov, &sdt_prov_list, prov_entry, tmp) {
 		ret = dtrace_unregister(prov->id);
