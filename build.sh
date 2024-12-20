f5491330b02d5c1d8d74be19118ca195d9814e2e
Ruslan Bukin <br@bsdpad.com>
2024-12-20 17:19:26 +0000

    Fix IP address

--- build.sh
+++ build.sh
@@ -3,21 +3,23 @@ if [ "$1" = "fast" ]; then
 	A=FAST
 fi
 
+IP_ADDR=10.5.0.155
+
 #make -j12 TARGET=arm64 KERN${A}=GENERIC-MORELLO cleankernel || exit 1
 make -j12 TARGET=arm64 KERN${A}=GENERIC-MORELLO buildkernel || exit 1
 
 cp /usr/obj/usr/home/br/dev/freebsd-morello/arm64.aarch64/sys/GENERIC-MORELLO/kernel /tftpboot/root/boot/kernel/kernel || exit 2
 
-scp /usr/obj/usr/home/br/dev/freebsd-morello/arm64.aarch64/sys/GENERIC-MORELLO/kernel 10.2.0.156:~/modules/boot/kernel/ || exit 3
+scp /usr/obj/usr/home/br/dev/freebsd-morello/arm64.aarch64/sys/GENERIC-MORELLO/kernel ${IP_ADDR}:~/modules/boot/kernel/ || exit 3
 
 # Coresight
-scp /usr/obj/usr/home/br/dev/freebsd-morello/arm64.aarch64/sys/GENERIC-MORELLO/modules/usr/home/br/dev/freebsd-morello/sys/modules/coresight/*/*ko 10.2.0.156:~/modules/ || exit 4
+scp /usr/obj/usr/home/br/dev/freebsd-morello/arm64.aarch64/sys/GENERIC-MORELLO/modules/usr/home/br/dev/freebsd-morello/sys/modules/coresight/*/*ko ${IP_ADDR}:~/modules/ || exit 4
 
 # SPE
-scp /usr/obj/usr/home/br/dev/freebsd-morello/arm64.aarch64/sys/GENERIC-MORELLO/modules/usr/home/br/dev/freebsd-morello/sys/modules/spe/spe.ko 10.2.0.156:~/modules/ || exit 4
+scp /usr/obj/usr/home/br/dev/freebsd-morello/arm64.aarch64/sys/GENERIC-MORELLO/modules/usr/home/br/dev/freebsd-morello/sys/modules/spe/spe.ko ${IP_ADDR}:~/modules/ || exit 4
 
 # hwt.ko
-scp /usr/obj/usr/home/br/dev/freebsd-morello/arm64.aarch64/sys/GENERIC-MORELLO/modules/usr/home/br/dev/freebsd-morello/sys/modules/hwt/hwt.ko 10.2.0.156:~/modules/ || exit 5
+scp /usr/obj/usr/home/br/dev/freebsd-morello/arm64.aarch64/sys/GENERIC-MORELLO/modules/usr/home/br/dev/freebsd-morello/sys/modules/hwt/hwt.ko ${IP_ADDR}:~/modules/ || exit 5
 
 # hwt
-scp /usr/obj/usr/home/br/dev/freebsd-morello/arm64.aarch64/usr.sbin/hwt/hwt 10.2.0.156:~/
+scp /usr/obj/usr/home/br/dev/freebsd-morello/arm64.aarch64/usr.sbin/hwt/hwt ${IP_ADDR}:~/
