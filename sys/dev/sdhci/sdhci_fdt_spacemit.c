e26b130c6d5219d77488a7cc247eba3a97512911
Bojan Novković <bnovkov@FreeBSD.org>
2026-06-18 20:45:36 +0200

    wip mmc

--- sys/dev/sdhci/sdhci_fdt_spacemit.c
+++ sys/dev/sdhci/sdhci_fdt_spacemit.c
@@ -12,7 +12,10 @@
 #include <sys/taskqueue.h>
 #include <sys/module.h>
 
+
 #include <machine/bus.h>
+#include <sys/rman.h>
+#include <machine/resource.h>
 
 #include <dev/ofw/ofw_bus.h>
 #include <dev/ofw/ofw_bus_subr.h>
@@ -35,6 +38,17 @@
 #include "opt_mmccam.h"
 #include "opt_soc.h"
 
+#define PHY_CTRL_REG    0x160
+#define  PHY_FUNC_EN    (1 << 0)
+#define  PHY_PLL_LOCK   (1 << 1)
+#define MMC_CTRL_REG    0x114
+#define  MMC_MODE       (1 << 12)
+#define  ENHANCE_STROBE_EN (1 << 8)
+#define TX_CFG_REG      0x11C
+#define  TX_INT_CLK_SEL (1 << 30)
+#define PHY_PADCFG_REG	0x178
+#define  RX_BIAS_SEL	(1 << 5)
+
 static struct ofw_compat_data compat_data[] = {
 	{ "spacemit,k1-sdhci", 1 },
 	{ NULL, 0 }
@@ -50,12 +64,6 @@ sdhci_fdt_spacemit_probe(device_t dev)
 	if (ofw_bus_search_compatible(dev, compat_data)->ocd_data == 0)
 		return (ENXIO);
 
-	if (ofw_bus_has_prop(dev, "no-sd")) {
-		/* XXX: only attach to the SD card for now. */
-		device_printf(dev, "MMC and SDIO aren't supported yet\n");
-		return (ENXIO);
-	}
-
 	device_set_desc(dev, "SpacemiT K1 SDHCI controller");
 
 	return (BUS_PROBE_DEFAULT);
@@ -65,12 +73,14 @@ static int
 sdhci_fdt_spacemit_attach(device_t dev)
 {
 	struct sdhci_fdt_softc *sc;
+	struct resource *res;
 	clk_t clk_core;
 	hwreset_t rst;
 
 	sc = device_get_softc(dev);
 	sc->quirks = SDHCI_QUIRK_PRESET_VALUE_BROKEN |
-		SDHCI_QUIRK_BROKEN_TIMEOUT_VAL;
+		SDHCI_QUIRK_BROKEN_TIMEOUT_VAL |
+		SDHCI_QUIRK_PRESET_VALUE_BROKEN;
 
 	if (clk_get_by_ofw_name(dev, 0, "core", &clk_core)) {
 		device_printf(dev, "cannot get core clock\n");
@@ -80,6 +90,7 @@ sdhci_fdt_spacemit_attach(device_t dev)
 		device_printf(dev, "cannot get io clock\n");
 		return (ENXIO);
 	}
+
 	if (hwreset_get_by_ofw_name(dev, 0, "sdh", &rst) != 0) {
 		device_printf(dev, "cannot get device reset\n");
 		return (ENXIO);
@@ -98,13 +109,42 @@ sdhci_fdt_spacemit_attach(device_t dev)
 		return (ENXIO);
 	}
 
+	if (!ofw_bus_has_prop(dev, "no-mmc")) {
+		reg = bus_read_4(res, MMC_CTRL_REG);
+		device_printf(dev, "%s: MMC_CTRL_REG: 0x%x\n", __func__, reg);
+		reg &= ~ENHANCE_STROBE_EN;
+		reg |= MMC_MODE;
+		bus_write_4(res, MMC_CTRL_REG, reg);
+
+		reg = bus_read_4(res, PHY_CTRL_REG);
+		device_printf(dev, "%s: PHY_CTRL_REG: 0x%x\n", __func__, reg);
+		reg |= PHY_FUNC_EN | PHY_PLL_LOCK;
+		bus_write_4(res, PHY_CTRL_REG, reg);
+
+		reg = bus_read_4(res, PHY_PADCFG_REG);
+		device_printf(dev, "%s: PHY_PADCFG_REG: 0x%x\n", __func__, reg);
+		reg |= RX_BIAS_SEL;
+		bus_write_4(res, PHY_PADCFG_REG, reg);
+	} else {
+		reg = bus_read_4(res, MMC_CTRL_REG);
+		device_printf(dev, "%s: MMC_CTRL_REG: 0x%x\n", __func__, reg);
+		reg &= ~ENHANCE_STROBE_EN;
+		bus_write_4(res, MMC_CTRL_REG, reg);
+
+		bus_write_4(res, MMC_CTRL_REG, reg);
+		reg = bus_read_4(res, TX_CFG_REG);
+		reg |= TX_INT_CLK_SEL;
+		bus_write_4(res, TX_CFG_REG, reg);
+	}
+	bus_release_resource(dev, res);
+
 	return (sdhci_fdt_attach(dev));
 }
 
 static int
 sdhci_fdt_spacemit_set_clock(device_t dev, struct sdhci_slot *slot, int clock)
 {
-	struct sdhci_fdt_softc *sc = device_get_softc(dev);
+	struct sdhci_fdt_softc *sc;
 	uint64_t freq;
 
 	sc = device_get_softc(dev);
